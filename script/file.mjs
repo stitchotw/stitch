19d9b3a64feb643f3ac60e608014859b98aa4f90
stitchotw <stitchotw@gmail.com>
2021-05-27 22:54:45 +0200

    Added empty click methods for all file buttons

--- script/file.mjs
+++ script/file.mjs
@@ -1,8 +1,24 @@
 class ButtonClickHandler {
 
-    fileNewPatternButtonClick() {
-        console.log("New pattern");
-    }
+    fileNewPatternButtonClick() { console.log("New pattern"); }
+
+    fileOpenPatternButtonClick() { console.log("Open pattern"); }
+
+    fileImportImageButtonClick() { console.log("Import image"); }
+
+    fileSaveToLocalStorageButtonClick() { console.log("Save to local storage"); }
+
+    fileSaveToFileButtonClick() { console.log("Save to file"); }
+
+    filePrintPatternButtonClick() { console.log("Print pattern"); }
+
+    fileExportToPngButtonClick() { console.log("Export PNG"); }
+
+    fileExportToSvgButtonClick() { console.log("Export SVG"); }
+
+    fileExportToHtmlButtonClick() { console.log("Export HTML"); }
+
+    fileExportToPearlButtonClick() { console.log("Export pearl boards"); }
 
 }
 
